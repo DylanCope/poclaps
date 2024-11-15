5d2a72f6cd3db8885f257b0db502528693dd671c
Dylan Cope <dylanr.cope@gmail.com>
2024-11-15 06:52:09 -0800

    prelim results from somewhat working learning method

---
+++ figures/prelim_run_results/goal_pos_count_table/code.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from reproducible_figures.plotting import set_plotting_style
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+matplotlib.use("pdf")
+
+
+def plot_count_table(msg_acc_df):
+    msg_acc_df["Goal X"] = msg_acc_df["Message"] // env_params.grid_size
+    msg_acc_df["Goal Y"] = msg_acc_df["Message"] % env_params.grid_size
+
+    count_table = (
+        msg_acc_df.groupby(["Goal X", "Goal Y"])["Accuracy"]
+        .count()
+        .reset_index()
+        .pivot(index="Goal X", columns="Goal Y", values="Accuracy")
+    )
+    count_table = count_table / count_table.sum().sum()
+
+    set_plotting_style()
+
+    ax = sns.heatmap(
+        np.float32(count_table.to_numpy()), cmap="viridis", cbar=True, annot=True
+    )
+    ax.set_xlabel("Goal X")
+    ax.set_ylabel("Goal Y")
+
+
+def reproduce_figure():
+    data = [
+        pd.read_csv(csv_path)
+        for csv_path in Path("figures/prelim_run_results/goal_pos_count_table").glob("data_*.csv")
+    ]
+    plot_count_table(*data)
+    plt.savefig(
+        "figures/prelim_run_results/goal_pos_count_table/goal_pos_count_table.pdf",
+        bbox_inches="tight",
+        dpi=1000,
+    )
+
+
+if __name__ == "__main__":
+    reproduce_figure()
---
+++ figures/prelim_run_results/transition_model_loss/code.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from reproducible_figures.plotting import set_plotting_style
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+matplotlib.use("pdf")
+
+
+def plot(df):
+    set_plotting_style()
+    ax = sns.lineplot(df, x="step", y="loss")
+    ax.set_xlabel("Training Step")
+    ax.set_ylabel("Loss")
+
+
+def reproduce_figure():
+    data = [
+        pd.read_csv(csv_path)
+        for csv_path in Path("figures/prelim_run_results/transition_model_loss").glob("data_*.csv")
+    ]
+    plot(*data)
+    plt.savefig(
+        "figures/prelim_run_results/transition_model_loss/transition_model_loss.pdf",
+        bbox_inches="tight",
+        dpi=1000,
+    )
+
+
+if __name__ == "__main__":
+    reproduce_figure()
